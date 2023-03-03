74bff20f80b91fc72ba32a0bf172cf5e44a2fbb5
Justin Bonnici <justin-edward.bonnici@gov.mt>
2023-03-03 10:53:05 +0100

    Initial Commit

---
+++ simple-betting-api/src/main/kotlin/com/betting/simplebettingapi/SimpleBettingApiApplication.kt
@@ -0,0 +1,11 @@
+package com.betting.simplebettingapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleBettingApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimpleBettingApiApplication>(*args)
+}
---
+++ simple-betting-api/src/test/kotlin/com/betting/simplebettingapi/SimpleBettingApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.betting.simplebettingapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleBettingApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
