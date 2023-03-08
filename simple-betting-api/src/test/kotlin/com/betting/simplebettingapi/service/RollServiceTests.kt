e7b114ff13b883e76c3a43fff05e0ada302de341
jbonn360 <justinbonnici94@gmail.com>
2023-03-08 20:33:14 +0100

    Add roll postponed unit test.

--- simple-betting-api/src/test/kotlin/com/betting/simplebettingapi/service/RollServiceTests.kt
+++ simple-betting-api/src/test/kotlin/com/betting/simplebettingapi/service/RollServiceTests.kt
@@ -9,6 +9,7 @@ import com.betting.simplebettingapi.model.RollModel
 import com.betting.simplebettingapi.model.WalletModel
 import com.betting.simplebettingapi.repository.BetRepository
 import com.betting.simplebettingapi.repository.RollRepository
+import io.mockk.called
 import io.mockk.every
 import org.junit.jupiter.api.Test
 
@@ -17,8 +18,8 @@ import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import java.math.BigDecimal
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 
-//@DataJpaTest
 class RollServiceTests() {
     // mock required classes
     private val walletService: WalletService = mockk()
@@ -33,9 +34,9 @@ class RollServiceTests() {
 
     @Test
     fun givenWinningCondition_WhenNumberIsRolled_AccountWinsBet() {
+        //given
         val numberBetOn: Byte = 5
 
-        //given
         val rollModel = RollModel(Instant.now(), 0)
         val walletModel = WalletModel(BigDecimal.ZERO)
         val accountModel = AccountModel("username", "name", "surname", walletModel)
@@ -43,8 +44,7 @@ class RollServiceTests() {
         walletModel.account = accountModel
 
         val betModel = BetModel(
-            BigDecimal(50), numberBetOn, BetStatus.PLACED, Instant.now(), rollModel,
-            accountModel
+            BigDecimal(50), numberBetOn, BetStatus.PLACED, Instant.now(), rollModel, accountModel
         )
 
         betModel.status = BetStatus.WON_10
@@ -63,19 +63,36 @@ class RollServiceTests() {
         // then
         // roll entity is updated
         rollModel.number = numberBetOn
-        verify { rollRepository.save(rollModel) }
+        verify { rollRepository.save(withArg { assertEquals(rollModel, it) }) }
 
         // bet entity is updated
-        verify { betRepository.save(withArg { bet -> assertEquals(BetStatus.WON_10, bet.status) }) }
+        verify { betRepository.save(withArg { assertEquals(BetStatus.WON_10, it.status) }) }
 
-        // balance is updated
+        // winner is rewarded
         verify {
-            walletService.updateBalance(
-                withArg { assertEquals(walletModel, it) },
+            walletService.updateBalance(withArg { assertEquals(walletModel, it) },
                 withArg { assertEquals(BigDecimal(500), it) },
                 withArg { assertEquals(TransactionType.BET_WIN, it) }
             )
         }
+    }
+
+    @Test
+    fun givenNoBetsFound_WhenNumberIsRolled_RollIsPostponed() {
+        //given
+        val rollInterval: Byte = 1
+        val rollModel = RollModel(Instant.now(), 5)
+        every { rollRepository.save(any()) } returns rollModel
+        rollService = RollServiceImpl(walletService, rollRepository, betRepository, utils, rollInterval)
+
+        every { betRepository.findAllByRoll(any()) } returns emptyList()
+
+        //when
+        rollService.rollNextNumber()
+
+        //then
+        verify(atMost = 2) { rollRepository.save(any()) }
+        verify(exactly = 0) { utils.generateRandomNumberRoll() }
     }
 }
 
